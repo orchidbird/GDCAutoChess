83de21242b8037fb724ba845bc02e902f5292827
Formille <skcksgml@gmail.com>
2019-12-06 18:54:55 +0900

    Update Shop1.cs

--- Assets/Scenes/Shop1.cs
+++ Assets/Scenes/Shop1.cs
@@ -7,6 +7,7 @@ public class Shop1 : MonoBehaviour
 {
     public GameObject ShopButton;
     public GameObject[] Warehouse = new GameObject[8];
+    public GameObject[] Board = new GameObject[12];
 
 
 	void Start()
@@ -31,6 +32,8 @@ public class Shop1 : MonoBehaviour
         //창고 오브젝트 가져오기
         for (int i = 0; i < 8; i++)
             Warehouse[i] = GameObject.Find("유닛창고" + (i + 1).ToString());
+        for (int i = 0; i < 12; i++)
+            Board[i] = GameObject.Find("판" + (i + 1).ToString());
     }
 
     //상점에서 카드 고르기
@@ -98,7 +101,29 @@ public class Shop1 : MonoBehaviour
 							}
 						}
 					}
-				}
+                    for (int k = 0; k < 12; k++)
+                    {
+                        if (a.board[k] == a.num[i] && a.boardLevel[k] == 1)
+                        {
+                            a.board[k] = -1;
+                            a.boardLevel[k] = 0;        //창고에 있는 카드 레벨 0으로 초기화
+                            Board[k].GetComponent<Image>().sprite = Resources.Load("창고", typeof(Sprite)) as Sprite;
+                            Board[k].transform.GetChild(0).GetComponent<Image>().sprite = Resources.Load("창고", typeof(Sprite)) as Sprite;
+                            Board[k].transform.GetChild(0).GetChild(0).GetComponent<Image>().sprite = Resources.Load("창고", typeof(Sprite)) as Sprite; ;//창고가 비었을 때 다시 창고 이미지 보여주기
+
+                            //1성 카드들 중 첫번째 카드만 2성 이미지로 보여주기
+                            if (b == 0)
+                            {
+                                a.board[k] = a.num[i];      // ??
+                                a.boardLevel[k] = 2;        //창고에 있는 카드레벨 2로 만들기
+                                Board[k].GetComponent<Image>().sprite = Resources.Load("카드배경_2", typeof(Sprite)) as Sprite; //카드 이미지 보여주기
+                                Board[k].transform.GetChild(0).GetComponent<Image>().sprite = Resources.Load(a.num[i].ToString(), typeof(Sprite)) as Sprite;
+                                Board[k].transform.GetChild(0).GetChild(0).GetComponent<Image>().sprite = Resources.Load("카드템플릿_2", typeof(Sprite)) as Sprite;
+                                b++;        // b=0일때만 작업 수행하기 때문에, 그 뒤로 발견되는 1성 카드들은 그냥 사라지기만 할 것이다.
+                            }
+                        }
+                    }
+                }
 
 				//2성 카드 3개면 3성 하나 만들기 (2성카드 하나 만드는 거랑 동일)
 				if (a.playerunitlevel2[a.num[i] - 1] == 3)
@@ -126,7 +151,29 @@ public class Shop1 : MonoBehaviour
 							}
 						}
 					}
-				}
+                    for (int k = 0; k < 12; k++)
+                    {
+                        if (a.board[k] == a.num[i] && a.boardLevel[k] == 2)
+                        {
+                            a.board[k] = -1;
+                            a.boardLevel[k] = 0;        //창고에 있는 카드 레벨 0으로 초기화
+                            Board[k].GetComponent<Image>().sprite = Resources.Load("창고", typeof(Sprite)) as Sprite;
+                            Board[k].transform.GetChild(0).GetComponent<Image>().sprite = Resources.Load("창고", typeof(Sprite)) as Sprite;
+                            Board[k].transform.GetChild(0).GetChild(0).GetComponent<Image>().sprite = Resources.Load("창고", typeof(Sprite)) as Sprite; ;//창고가 비었을 때 다시 창고 이미지 보여주기
+
+                            //1성 카드들 중 첫번째 카드만 2성 이미지로 보여주기
+                            if (b == 0)
+                            {
+                                a.board[k] = a.num[i];      // ??
+                                a.boardLevel[k] = 3;        //창고에 있는 카드레벨 3로 만들기
+                                Board[k].GetComponent<Image>().sprite = Resources.Load("카드배경_3", typeof(Sprite)) as Sprite; //카드 이미지 보여주기
+                                Board[k].transform.GetChild(0).GetComponent<Image>().sprite = Resources.Load(a.num[i].ToString(), typeof(Sprite)) as Sprite;
+                                Board[k].transform.GetChild(0).GetChild(0).GetComponent<Image>().sprite = Resources.Load("카드템플릿_3", typeof(Sprite)) as Sprite;
+                                b++;        // b=0일때만 작업 수행하기 때문에, 그 뒤로 발견되는 1성 카드들은 그냥 사라지기만 할 것이다.
+                            }
+                        }
+                    }
+                }
 				ShopButton.SetActive(!ShopButton.active);  //ShopButton 활성화 여부 변경
 				a.shop[i] = false;  //shop = 골라진 칸
 				break;
